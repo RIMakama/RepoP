5407b8978d2a2225e5b6190988f4f9630d582171
RIMakama <221494316@edu.vut.ac.za>
2024-08-27 12:07:37 +0200

    code added

--- RepoP.py
+++ RepoP.py
@@ -1,1 +1,9 @@
-py_file
+print("Hi, How are you doing ")
+name=input ("Who can i call you ")
+print ("oh "+name +" how are you doing")
+response =input()
+
+if  response=="good":
+    print ("that is great")
+else:
+    print ("tell me what is wrong ")
